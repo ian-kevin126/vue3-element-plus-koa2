7f87a5b23c61d91e085ae2277f4c99ac8340f664
ian_kevin126 <ian_kevin126@126.com>
2021-11-24 16:14:12 +0800

    feat: 封装server端日志系统

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const log4js = require('./utils/log4j')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -13,23 +14,37 @@ const users = require('./routes/users')
 onerror(app)
 
 // middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
+app.use(
+  bodyparser({
+    enableTypes: ['json', 'form', 'text'],
+  })
+)
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
+app.use(
+  views(__dirname + '/views', {
+    extension: 'pug',
+  })
+)
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+  // const start = new Date()
+  // await next()
+  // const ms = new Date() - start
+  // console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+  log4js.info(`get params:${JSON.stringify(ctx.request.query)}`)
+  log4js.info(`post params:${JSON.stringify(ctx.request.body)}`)
+  await next().catch((err) => {
+    if (err.status == '401') {
+      ctx.status = 200
+      ctx.body = util.fail('Token认证失败', util.CODE.AUTH_ERROR)
+    } else {
+      throw err
+    }
+  })
 })
 
 // routes
@@ -37,8 +52,13 @@ app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
 
 // error-handling
+// app.on('error', (err, ctx) => {
+//   console.error('server error', err, ctx)
+// });
+
+// error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
-});
+  log4js.error(`${err.stack}`)
+})
 
 module.exports = app
